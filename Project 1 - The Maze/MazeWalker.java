35555c4a85ad3e654f15eb3dea6b6c7d2d2f00b8
nicolegreenberg <784517@seq.org>
2016-08-30 08:39:54 -0700

    Add files via upload

--- Project 1 - The Maze/MazeWalker.java
+++ Project 1 - The Maze/MazeWalker.java
@@ -7,6 +7,15 @@
  */
 public class MazeWalker
 {
+   
+    public static void run()
+    {
+        Maze maze = new Maze();
+        MazeWalker mazeWalker = new MazeWalker();
+        mazeWalker.walkMaze(maze.getMazeBot());
+    }
+
+
     // instance variables - replace the example below with your own
     private int x;
 
@@ -32,29 +41,22 @@ public class MazeWalker
     }
     public void walkMaze(MazeBot mazeBot) {
         // Your instructions go here
-        mazeBot.moveForward();
-        mazeBot.moveForward();
-        mazeBot.turnLeft();
-        mazeBot.moveForward();
-        mazeBot.moveForward();
-        mazeBot.moveForward();
-        mazeBot.turnRight();
-        mazeBot.moveForward();
-        mazeBot.moveForward();
-        mazeBot.turnRight();
-        mazeBot.moveForward();
-        mazeBot.moveForward();
-        mazeBot.moveForward();
-        mazeBot.moveForward();
-        mazeBot.turnLeft();
-        mazeBot.moveForward();
-        mazeBot.moveForward();
-        mazeBot.moveForward();
-        mazeBot.turnLeft();
-        mazeBot.moveForward();
-        mazeBot.moveForward();
-        mazeBot.turnRight();
-        mazeBot.moveForward();
-        mazeBot.moveForward();
+        while (mazeBot.canMoveForward()){
+            mazeBot.moveForward();
+            if (mazeBot.canMoveForward() == false){
+                mazeBot.turnRight();
+                if (mazeBot.canMoveForward() == false){
+                    mazeBot.turnLeft();
+                    mazeBot.turnLeft();
+        }
+    }
+}
+       
+        if (mazeBot.didReachGoal()) {
+            mazeBot.signalSuccess();
+        }
+        else if (mazeBot.didNotReachGoal()){
+            mazeBot.signalError();
+        }
     }
 }
