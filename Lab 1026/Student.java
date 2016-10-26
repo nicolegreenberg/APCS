0ed13b4877bcb6a5647fd8a0bb798bafcafe3b26
nicolegreenberg <784517@seq.org>
2016-10-26 08:47:43 -0700

    Add files via upload

---
+++ Lab 1026/Student.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class Student here.
+ * 
+ * Nicole Greenberg 
+ * @version (a version number or a date)
+ */
+public class Student
+{
+    int stuID;
+    double stuGPA;
+    String stuName;
+    public Student(int id, double gpa, String name)
+    {
+        stuID = id;
+        stuGPA = gpa;
+        stuName = name;
+    }
+
+    public int getStuID(int stuID)
+    {
+        return stuID;
+    }
+
+    public double getStuGPA(double stuGPA){
+        return stuGPA;
+    }
+
+    public String getStuName(String stuName){
+        return stuName;
+    }
+
+    public int setStuID(int x){
+        stuID = x;
+    }
+
+    public double setStuGPA(double y){
+        stuGPA = y;
+    }
+
+    public String setStuName(String z){
+        stuName = z;
+    }
+}
